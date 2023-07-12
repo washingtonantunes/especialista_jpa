6d542e9254411d2afb207df75fddb803f0e85cad
Washington Antunes <washington.cead@gmail.com>
2023-07-12 07:45:57 -0300

    criando projeto

---
+++ src/main/java/br/com/wti/Main.java
@@ -0,0 +1,11 @@
+package br.com.wti;
+
+/**
+ * @author Washington Antunes for wTI on ${DATE}
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
